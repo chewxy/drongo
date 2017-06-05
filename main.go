7fcb381a7a2440e3a1d6e693d07f44bcbdd98608
chewxy <chewxy@gmail.com>
2017-06-05 18:10:11 +1000

    Updated training model, added a predict method
    I think basic training now works fine

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"math/rand"
 
 	"github.com/chewxy/gorgonia"
 	"github.com/chewxy/gorgonia/tensor"
@@ -10,6 +12,11 @@ import (
 
 const (
 	MAXQUERY = 45 // query length of 45 words is max. You can't analyze idiots like Nabokov or James Joyce but those are shitty writers anyway
+)
+
+const (
+	lib001 = `As usual, then, government intervention into the market caused unintended, undesired consequences, but politicians blame the HMOs instead of the interventions that helped create them.`
+	con001 = `They have transferred control of elections to the government bureaucracy they fund and control, created complex ballot-access laws, switched to the Australian ballot to weaken local parties, outlawed corporate contributions, and imposed contribution limits to make it hard for opponents to fund a credible challenge.`
 )
 
 func main() {
@@ -20,15 +27,61 @@ func main() {
 	if err := loadExamples(); err != nil {
 		log.Fatal(err)
 	}
+	shuffleExamples(examples)
+	shuffleExamples(examples)
+	shuffleExamples(examples)
+
+	trainingLen := int(0.85 * float64(len(examples)))
+	trainingSet := make([]example, trainingLen)
+	copy(trainingSet, examples)
 
 	emb := depModel.WordEmbeddings()
 	m := NewModel(emb.Shape(), tensor.Float64, MAXQUERY, int(MAXTARGETS))
 	m.c = depModel.Corpus()
 	m.SetEmbed(emb)
-	solver := gorgonia.NewAdamSolver()
-	for i := 0; i < 100; i++ {
-		if err := m.Train(i, solver); err != nil {
+	solver := gorgonia.NewAdamSolver(gorgonia.WithClip(3.0), gorgonia.WithLearnRate(0.0005), gorgonia.WithL2Reg(0.000001))
+	for i := 0; i < 300; i++ {
+		if err := Train(i, m, solver, trainingSet); err != nil {
 			log.Fatalf("Error while training during iteration %d: %+v", i, err)
 		}
+		shuffleExamples(trainingSet)
 	}
+
+	t0, err := m.Pred(lib001)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t1, err := m.Pred(con001)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("T0: %v | T1: %v\n", t0, t1)
+}
+
+func Train(epoch int, m *Model, solver gorgonia.Solver, trainingSet []example) (err error) {
+	costs := make([]float64, len(trainingSet))
+	for i, ex := range trainingSet {
+		var cost float64
+		if cost, err = m.Train(solver, ex); err != nil {
+			return
+		}
+		costs[i] = cost
+	}
+	log.Printf("Epoch %d. Avg Cost %f", epoch, averageCosts(costs))
+	return nil
+}
+
+func shuffleExamples(a []example) {
+	for i := range a {
+		j := rand.Intn(i + 1)
+		a[i], a[j] = a[j], a[i]
+	}
+}
+
+func averageCosts(a []float64) (retVal float64) {
+	for _, v := range a {
+		retVal += v
+	}
+	return retVal / float64(len(a))
 }
--- model.go
+++ model.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"io/ioutil"
-	"log"
-	"math/rand"
+	"strings"
 
 	. "github.com/chewxy/gorgonia"
 	"github.com/chewxy/gorgonia/tensor"
@@ -72,7 +71,7 @@ func (m *Model) WordID(a *lingo.Annotation) int {
 	return id
 }
 
-func (m *Model) Fwd(wordID int, prev *Node) (h, e *Node, err error) {
+func (m *Model) OneWord(wordID int, prev *Node) (h, e *Node, err error) {
 	if err = UnsafeLet(m.input, S(wordID)); err != nil {
 		return
 	}
@@ -88,7 +87,7 @@ func (m *Model) Fwd(wordID int, prev *Node) (h, e *Node, err error) {
 	return
 }
 
-func (m *Model) CostFn(s lingo.AnnotatedSentence, target Target) (cost *Node, err error) {
+func (m *Model) Fwd(s lingo.AnnotatedSentence) (prob *Node, err error) {
 	hiddens := make(Nodes, 0, len(s))
 	exps := make(Nodes, 0, len(s))
 	var runningSum *Node
@@ -100,7 +99,7 @@ func (m *Model) CostFn(s lingo.AnnotatedSentence, target Target) (cost *Node, er
 		}
 
 		var h, e *Node
-		if h, e, err = m.Fwd(m.WordID(a), prev); err != nil {
+		if h, e, err = m.OneWord(m.WordID(a), prev); err != nil {
 			return
 		}
 
@@ -138,22 +137,24 @@ func (m *Model) CostFn(s lingo.AnnotatedSentence, target Target) (cost *Node, er
 		}
 	}
 
-	var prob *Node
-	if prob, err = Mul(m.p, context); err != nil {
-		err = errors.Wrap(err, "Context Mul")
+	var finalLayer *Node
+	if finalLayer, err = Mul(m.p, context); err != nil {
 		return
 	}
-
-	logProb := Must(Neg(Must(Log(prob))))
-	cost = Must(Slice(logProb, S(int(target))))
-	return
+	return SoftMax(finalLayer)
 }
 
-func (m *Model) Train(iter int, solver Solver) (err error) {
-	log.Printf("iter %d | %t", iter, m == nil)
-	i := rand.Intn(len(examples))
-	pair := examples[i]
+func (m *Model) CostFn(s lingo.AnnotatedSentence, target Target) (cost *Node, err error) {
+	var prob *Node
+	if prob, err = m.Fwd(s); err != nil {
+		err = errors.Wrap(err, "FWD")
+		return
+	}
+	logProb := Must(Neg(Must(Log(prob))))
+	return Slice(logProb, S(int(target)))
+}
 
+func (m *Model) Train(solver Solver, pair example) (c float64, err error) {
 	var g *ExprGraph
 	var cost *Node
 	if cost, err = m.CostFn(pair.dep.AnnotatedSentence, pair.target); err != nil {
@@ -173,7 +174,44 @@ func (m *Model) Train(iter int, solver Solver) (err error) {
 		}
 		return
 	}
+
+	v := cost.Value()
+	switch v.Dtype() {
+	case Float32:
+		c = float64(v.Data().(float32))
+	case Float64:
+		c = v.Data().(float64)
+	}
+
 	machine.UnbindAll()
 
-	return solver.Step(m.Learnables())
+	err = solver.Step(m.Learnables())
+	return
+}
+
+func (m *Model) Pred(s string) (class Target, err error) {
+	var dep *lingo.Dependency
+	if dep, err = pipeline(s, strings.NewReader(s)); err != nil {
+		err = errors.Wrap(err, "Basic NLP pipeline failed")
+		return
+	}
+
+	var prob *Node
+	if prob, err = m.Fwd(dep.AnnotatedSentence); err != nil {
+		err = errors.Wrap(err, "Fwd failed")
+		return
+	}
+	g := m.g.SubgraphRoots(prob)
+	machine := NewLispMachine(g, ExecuteFwdOnly())
+	if err = machine.RunAll(); err != nil {
+		return
+	}
+
+	val := prob.Value().(tensor.Tensor)
+
+	var t tensor.Tensor
+	if t, err = tensor.Argmax(val, 0); err != nil {
+		return
+	}
+	return Target(t.ScalarValue().(int)), nil
 }
